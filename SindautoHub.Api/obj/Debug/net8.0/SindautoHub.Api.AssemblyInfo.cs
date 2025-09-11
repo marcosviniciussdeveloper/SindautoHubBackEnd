8bdfaede6bae52f6b7306ee9a6bfd8a3f9e3acbf
Marcos <marcos.viniciuss.2102@gmail.com>
2025-09-11 18:01:44 -0300

    fix: Corrigindo as tabelas do banco

--- SindautoHub.Api/obj/Debug/net8.0/SindautoHub.Api.AssemblyInfo.cs
+++ SindautoHub.Api/obj/Debug/net8.0/SindautoHub.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SindautoHub.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f734190bb4ba31d7d61f85f9b4c4ba5e9b8800af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5248781b404a2ae46b7708710a1a294c5c56fc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SindautoHub.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SindautoHub.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
